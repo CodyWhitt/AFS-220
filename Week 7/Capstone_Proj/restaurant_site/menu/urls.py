99796cd6a1180496f357c3257ac3420161f7f86b
CodyWhitt <Whittcody@Yahoo.com>
2023-07-23 21:19:29 -0500

    week 5 submission

--- Week 7/Capstone_Proj/restaurant_site/menu/urls.py
+++ Week 7/Capstone_Proj/restaurant_site/menu/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('catering', views.catering, name='catering'),
+    path('info', views.info, name='info'),
+    path('menu', views.menu, name='menu'),
+    path('home', views.home, name='home'),
 ]
--- Week 7/Capstone_Proj/restaurant_site/menu/views.py
+++ Week 7/Capstone_Proj/restaurant_site/menu/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+def info(request):
+    return render(request, 'info.html')
+def catering(request):
+    return render(request, 'catering.html')
+def menu(request):
+    return render(request, 'menu.html')
